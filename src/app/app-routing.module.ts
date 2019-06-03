193d6aef9eb34ac8903511112c11d2390ee704f3
roedit <mail@andreitoma.com>
2019-06-03 21:25:15 +0100

    Create the router setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsListComponent } from './components/contacts/contacts-list/contacts-list.component';
+import { ContactsDetailsComponent } from './components/contacts/contacts-details/contacts-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/home'
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+        data: {
+          title: 'Home'
+        }
+      },
+      {
+        path: 'contacts',
+        component: ContactsListComponent,
+        data: {
+          title: 'Contacts'
+        }
+      },
+      {
+        path: 'contacts/:id',
+        component: ContactsDetailsComponent,
+        data: {
+          title: 'Contact details page'
+        }
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsListComponent } from './components/contacts/contacts-list/contacts-list.component';
+import { ContactsDetailsComponent } from './components/contacts/contacts-details/contacts-details.component';
+import { AddressFormComponent } from './components/contacts/address-form/address-form.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsListComponent,
+    ContactsDetailsComponent,
+    AddressFormComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
